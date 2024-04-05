2fedd085ae7a289b1713a73c7a938a13915ac85a
Gamarra Jose Maria <josegamarraformosa@gmail.com>
2024-04-05 14:01:52 -0300

    subir cambios

---
+++ json-empleados.js
@@ -0,0 +1,194 @@
+const empleados = [
+    {
+        "legajo": "601",
+        "usuario": "BEA",
+        "apellido": "BROCAZ, ",
+        "nombre": "enrique amadeo",
+        "area": "GA ALMACEN",
+        "cargo": "Respon Almacenes"
+    },
+    {
+        "legajo": "2734",
+        "usuario": "DAR",
+        "apellido": "RESQUIN, ",
+        "nombre": "daniel",
+        "area": "GA COMPRAS",
+        "cargo": "Responsable Compras"
+    },
+    {
+        "legajo": "2706",
+        "usuario": "CRA",
+        "apellido": "CABRERA, ",
+        "nombre": "ramon alejandro",
+        "area": "GA CONTABLE",
+        "cargo": "Gerente Admin y Finanzas"
+    },
+    {
+        "legajo": "3175",
+        "usuario": "CAM",
+        "apellido": "CANESÍN, ",
+        "nombre": "marcelo alejandro",
+        "area": "GA TESORERIA",
+        "cargo": "Responsable de Tesorería"
+    },
+    {
+        "legajo": "3333",
+        "usuario": "ARB",
+        "apellido": "BARBOSA, ",
+        "nombre": "alejandro ricardo",
+        "area": "GA TESORERIA",
+        "cargo": "Auxiliar de Tesorería"
+    },
+    {
+        "legajo": "3293",
+        "usuario": "MCZ",
+        "apellido": "ZUAREZ, ",
+        "nombre": "myrian",
+        "area": "GA CONTABLE",
+        "cargo": "Gestion de Impuestos"
+    },
+    {
+        "legajo": "3192",
+        "usuario": "YAA",
+        "apellido": "AYALA, ",
+        "nombre": "yolanda",
+        "area": "GA COMPRAS",
+        "cargo": "Auxiliar Compras"
+    },
+    {
+        "legajo": "3265",
+        "usuario": "MAB",
+        "apellido": "BELOSEVICH, ",
+        "nombre": "miryan adriana",
+        "area": "PR SEC. PRESIDENCIA",
+        "cargo": "Secretaria de Relac. Instituc. del Directorio"
+    },
+    {
+        "legajo": "3280",
+        "usuario": "JZA",
+        "apellido": "ZAMORELO, ",
+        "nombre": "juan javier",
+        "area": "GA ALMACEN",
+        "cargo": "Auxiliar de Almacenes"
+    },
+    {
+        "legajo": "3445",
+        "usuario": "JEB",
+        "apellido": "BENITEZ, ",
+        "nombre": "javier eduardo",
+        "area": "GA CONTABLE",
+        "cargo": "Sub Gerente Principal Adm. y Finanzas"
+    },
+    {
+        "legajo": "3415",
+        "usuario": "DMS",
+        "apellido": "SANCHEZ, ",
+        "nombre": "diego marcelo",
+        "area": "GA COMPRAS",
+        "cargo": "Auxiliar Administrativo"
+    },
+    {
+        "legajo": "3466",
+        "usuario": "PAR",
+        "apellido": "RIOS, ",
+        "nombre": "pamela",
+        "area": "GA TESORERIA",
+        "cargo": "Auxiliar Administrativo"
+    },
+    {
+        "legajo": "3503",
+        "usuario": "PAG",
+        "apellido": "ARZAMENDIA GONZALEZ, ",
+        "nombre": "pedro",
+        "area": "GA TESORERIA",
+        "cargo": "CAJERO"
+    },
+    {
+        "legajo": "3515",
+        "usuario": "JEF",
+        "apellido": "FORNARI, ",
+        "nombre": "juan eduardo",
+        "area": "GA TESORERIA",
+        "cargo": "CAJERO"
+    },
+    {
+        "legajo": "3578",
+        "usuario": "AMS",
+        "apellido": "SANCHEZ, ",
+        "nombre": "manuel alejandro",
+        "area": "GA CONTABLE",
+        "cargo": "Auxiliar Contable"
+    },
+    {
+        "legajo": "3580",
+        "usuario": "CMR",
+        "apellido": "RAMIREZ, ",
+        "nombre": "cristian manuel",
+        "area": "GA CONTABLE",
+        "cargo": "Auxiliar Contable"
+    },
+    {
+        "legajo": "3632",
+        "usuario": "ADM",
+        "apellido": "MARTINEZ, ",
+        "nombre": "alejandro daniel",
+        "area": "GA ALMACEN",
+        "cargo": "Auxiliar de Almacenes"
+    },
+    {
+        "legajo": "3633",
+        "usuario": "CRP",
+        "apellido": "PEREZ, ",
+        "nombre": "celso ricardo",
+        "area": "GA TESORERIA",
+        "cargo": "CAJERO"
+    },
+    {
+        "legajo": "3667",
+        "usuario": "DMA",
+        "apellido": "AUGUSTO MENDEZ, ",
+        "nombre": "david emmanuel",
+        "area": "GA COMPRAS",
+        "cargo": "Auxiliar Compras"
+    },
+    {
+        "legajo": "3684",
+        "usuario": "VFA",
+        "apellido": "ACOSTA, ",
+        "nombre": "victor fabian",
+        "area": "GA CONTABLE",
+        "cargo": "Auxiliar Contable"
+    },
+    {
+        "legajo": "3773",
+        "usuario": "CAR",
+        "apellido": "REGINI, ",
+        "nombre": "carmen maricel",
+        "area": "GA CONTABLE",
+        "cargo": "Auxiliar Contabilidad"
+    },
+    {
+        "legajo": "3800",
+        "usuario": "FGV",
+        "apellido": "VARGAS, ",
+        "nombre": "federico gabriel",
+        "area": "GA TESORERIA",
+        "cargo": "CAJERO"
+    },
+    {
+        "legajo": "3879",
+        "usuario": "RLG",
+        "apellido": "GAMARRA, ",
+        "nombre": "rodrigo alexis",
+        "area": "GA TESORERIA",
+        "cargo": "CAJERO"
+    },
+    {
+        "legajo": "3878",
+        "usuario": "MLR",
+        "apellido": "ROJAS , ",
+        "nombre": "maría laura",
+        "area": "GA CONTABLE",
+        "cargo": "Auxiliar Contable"
+    }
+];
